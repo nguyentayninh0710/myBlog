a1362914e7c2435cb0351b708d752074af7f9d91
Huynh Ky Khoi Nguyen (BD/XDV2) <hng5hc@bosch.com>
2023-08-23 14:57:03 +0700

    commit

--- src/main/java/com/myclass/blog/payload/GlobalExceptionHandler.java
+++ src/main/java/com/myclass/blog/payload/GlobalExceptionHandler.java
@@ -25,7 +25,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> handleGlobalException(BlogApiException exception, WebRequest webRequest){
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
